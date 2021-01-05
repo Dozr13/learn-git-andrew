7b7ad92dbf70418e9d9d7a9c8129575996a22d9f
Wade Pate <wadejp8@gmail.com>
2021-01-04 20:15:27 -0700

    Added some comments about Keyboard shortcuts

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 Some more cool stuff
 
 Added in some code
+
+// Focus File Explorer: Ctrl + Shift + E
+// Hide SIde Pane: Ctrl + B
+// Close Folder: Left arrow
+// Open Folder: Right Arrow
+// Open File: Ctrl + Enter
+// Close File: Ctrl + W
+// Go To File: Ctrl + P
+// Split Screen: Ctrl + \
+// Go To Group: Ctrl + 1/2/3
+// Change File In Group: Ctrl + Tab
